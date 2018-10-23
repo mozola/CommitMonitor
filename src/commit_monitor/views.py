5e35440354f910f5741156b51dd10ad361b1f60b
Mozola Waldemar <waldemar.mozola@nokia.com>
2018-10-23 21:28:09 +0200

    Implement new views

--- src/commit_monitor/views.py
+++ src/commit_monitor/views.py
@@ -1,3 +1,40 @@
-@app.route('/', methods=['POST'])
+import flask
+
+from flask import render_template
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
 def index():
-    return pass
+    return flask.render_template('base/index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+
+@app.route('/logout')
+def logout():
+    return render_template('auth/logout.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('auth/register.html')
+
+
+@app.route('/repository/add')
+def add_repository():
+    return render_template('repository/new.html')
+
+
+@app.route('/repository/display')
+def display_repository():
+    return render_template('repository/display.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404

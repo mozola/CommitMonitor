6c519dce8a5ad57266ba6f039915f277694b6316
WMozola <mozola.waldemar@gmail.com>
2018-10-28 19:13:28 +0100

    Add commits statistics (#2)
    Implement basic background

--- src/commit_monitor/http_handler.py
+++ src/commit_monitor/http_handler.py
@@ -9,27 +9,17 @@ class Repository:
     def __init__(self, name=None, url=None):
         self.name = name
         self.url = url
-        self.branches = self.get_branches()
+        self.branch = None
         self.commits = None
 
-    def _get_branches_names(self):
-        resp = requests.get(self.url + '/branches/')
+    def _get_commits(self, branch):
+        resp = requests.get('{}/commits/{}'.format(self.url, branch))
         soup = BeautifulSoup(resp.text)
-        for i in soup.findAll('div', {'class': 'branch-summary js-branch-row'}):
-            yield i.attrs['data-branch-name']
+        for i in soup.find_all('a', {'class': 'message js-navigation-open'}):
+            yield i.attrs['title']
 
-    def get_commits_branch(self):
-        for branch in self._get_branches_names():
-            commits = []
-            resp = requests.get('{}/commits/{}'.format(self.url, branch))
-            soup = BeautifulSoup(resp.text)
-            for i in soup.find_all('a', {'class': 'message js-navigation-open'}):
-               commits.append(i.attrs['title'])
-            yield len(commits)
-
-    def get_branches(self):
-        return zip(list(self._get_branches_names()),
-                   list(self.get_commits_branch()))
+    def get_commits(self, branch):
+        return list(self._get_commits(branch))
 
     def get_users(self):
         pass
@@ -39,6 +29,17 @@ class Repositories:
 
     def __init__(self):
         self._container = []
-    
+
+    def get_branches(self, url):
+        resp = requests.get(url + '/branches/')
+        soup = BeautifulSoup(resp.text)
+        for i in soup.findAll('div', {'class': 'branch-summary js-branch-row'}):
+            yield i.attrs['data-branch-name']
+
     def add(self, repository):
-        self._container.append(repository)
+        for branch in self.get_branches(repository.url):
+            print(branch)
+            repo = Repository(repository.name, repository.url)
+            repo.branch = branch
+            repo.commits = repository.get_commits(branch)
+            self._container.append(repo)
